88faf481665bad6c83760d95f74f75c806ee147f
itsyashanky <shanky392002@gmail.com>
2024-03-05 01:43:01 +0530

    first changes

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Shashank Portfolio',
+    description: 'Portfolio website for my Personal Projects.',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
